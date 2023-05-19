62c470c8264fd1c556f845acfd1c5296d68f45af
AlbertoArochal <arochaldev@gmail.com>
2023-05-19 18:41:16 +0200

    added entry and entrylist components

---
+++ src/modules/daybook/components/entry.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="entry-container mb-3 pointer p-2">
+    <div class="entry-title d-flex">
+      <span class="text-success fs-5 fw-bold"> 12 </span>
+      <span class="mx-1 fs-5"> julio </span>
+      <span class="mx-2 fw-light"> 2021, jueves</span>
+    </div>
+    <div class="entry-description">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
+      inventore, eius expedita molestias quibusdam eveniet libero quas
+      consequatur neque. Amet porro magnam eligendi officiis possimus excepturi
+      eum ipsum nesciunt facere!
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "EntryComponent",
+};
+</script>
+
+<style lang="scss">
+.entry-container {
+  background-color: #fff;
+  border-radius: 10px;
+  box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.25);
+  transition: all 0.2s ease-in;
+  &:hover {
+    box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.75);
+    background-color: lighten($color: grey, $amount: 45);
+    transition: all 0.2s ease-in;
+  }
+  .entry-description {
+    font-size: 12px;
+  }
+}
+</style>
---
+++ src/modules/daybook/components/entrylist.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="entry-list-container">
+    <div class="px-2 pt">
+      <input type="text" class="form-control" placeholder="Buscar Entradas" />
+    </div>
+    <div class="entry-scrollarea">
+      <h2 v-for="item in 100" :key="item">
+        <entry />
+      </h2>
+    </div>
+  </div>
+</template>
+<script>
+import { defineAsyncComponent } from "vue";
+export default {
+  name: "EntryList",
+  components: {
+    entry: defineAsyncComponent(() => import("../components/entry.vue")),
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.entry-list-container {
+  border-right: 1px solid #2c3e50;
+  background-color: #f3f3f3;
+  height: calc(100vh - 56px);
+  display: flex;
+  flex-direction: column;
+}
+
+.entry-scrollarea {
+  flex: 1;
+  overflow-y: scroll;
+  padding: 10px;
+  height: calc(100vh - 120px);
+}
+</style>
