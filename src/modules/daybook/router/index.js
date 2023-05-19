9bd566a80f279fdfede98c9e5636ee972c4a5b30
AlbertoArochal <arochaldev@gmail.com>
2023-05-19 20:45:55 +0200

    Fab Button added

--- src/modules/daybook/router/index.js
+++ src/modules/daybook/router/index.js
@@ -2,4 +2,27 @@ export default {
   name: "daybook",
   component: () =>
     import(/* webpackChunkName: "daybook" */ "../layouts/daybookLayout.vue"),
+  children: [
+    {
+      path: "",
+      name: "no-entry",
+      component: () =>
+        import(
+          /* webpackChunkName: "daybook-no-entry" */ "../views/noEntrySelected.vue"
+        ),
+    },
+    {
+      path: ":id",
+      name: "entry",
+      component: () =>
+        import(
+          /* webpackChunkName: "daybook-entry" */ "../views/entryView.vue"
+        ),
+      props: (route) => {
+        return {
+          id: route.params.id,
+        };
+      },
+    },
+  ],
 };
