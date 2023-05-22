c93c690a178b2f6aac894ce70ba527d1adaad880
AlbertoArochal <arochaldev@gmail.com>
2023-05-22 21:56:15 +0200

    added actions for using firebase rdb

--- src/modules/daybook/layouts/daybookLayout.vue
+++ src/modules/daybook/layouts/daybookLayout.vue
@@ -1,7 +1,16 @@
 <template>
   <navBar />
 
-  <div class="d-flex">
+  <div v-if="isLoading" class="row justify-content-md-center">
+    <div class="col-3 alert-info text-center mt-5">
+      "espere por favor..."
+      <h3 class="mt-2">
+        <i class="fa fa-spinner fa-spin"></i>
+      </h3>
+    </div>
+  </div>
+
+  <div v-else class="d-flex">
     <div class="col-4">
       <entryList />
     </div>
@@ -13,12 +22,22 @@
 
 <script>
 import { defineAsyncComponent } from "vue";
+import { mapActions, mapState } from "vuex";
 export default {
   components: {
     navBar: defineAsyncComponent(() => import("../components/navbar.vue")),
     entryList: defineAsyncComponent(() =>
       import("../components/entrylist.vue")
     ),
+  },
+  methods: {
+    ...mapActions("journal", ["loadEntries"]),
+  },
+  computed: {
+    ...mapState("journal", ["isLoading"]),
+  },
+  created() {
+    this.loadEntries();
   },
 };
 </script>
--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -1,25 +1,6 @@
 const state = () => ({
   isLoading: true,
-  entries: [
-    {
-      id: "1",
-      date: new Date().toDateString(),
-      text: "lorem ipsum dolor sit amet consectetur adipisicing elit.",
-      picture: null,
-    },
-    {
-      id: "2",
-      date: new Date().toDateString(),
-      text: "lorem ipsum dolor sit amet consectetur adipisicing elit. lorem ipsum dolor sit amet consectetur adipisicing elit. lorem ipsum dolor sit amet consectetur adipisicing elit. lorem ipsum dolor sit amet consectetur adipisicing elit. lorem ipsum dolor sit amet consectetur adipisicing elit.",
-      picture: null,
-    },
-    {
-      id: "3",
-      date: new Date().toDateString(),
-      text: "lorem ipsum dolor sit amet consectetur adipisicing elit.",
-      picture: null,
-    },
-  ],
+  entries: [],
 });
 
 export default state;
